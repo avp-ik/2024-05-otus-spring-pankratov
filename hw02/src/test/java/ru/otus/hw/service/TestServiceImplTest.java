f0607f1869e2a491cb7468c93e4081b49e573f74
avp-ik <alexander.pankratov@gmail.com>
2024-08-10 23:22:29 +0300

    Initial hw02 commit.

---
+++ hw02/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,28 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.dao.CsvQuestionDao;
+
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Класс TestServiceImpl")
+public class TestServiceImplTest {
+    @DisplayName("корректно создаётся конструктором")
+    @Test
+    void shouldHaveCorrectConstructor() {
+        PrintStream printStream = System.out;
+        StreamsIOService ioService = new StreamsIOService(printStream);
+
+        CsvQuestionDao questionDao = new CsvQuestionDao(new AppProperties("questionsTest.csv"));
+
+        TestServiceImpl testServiceImpl = new TestServiceImpl(ioService, questionDao);
+
+        assertTrue(ioService != null);
+        assertTrue(questionDao.findAll().size() == 3);
+        assertTrue(testServiceImpl != null);
+    }
+}
